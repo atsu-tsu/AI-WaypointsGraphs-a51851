1033f7581d0e5e78ad047cf859e3afd3765710ca
atsu-tsu <fdferreira93@gmail.com>
2023-10-28 20:33:19 +0100

    2.Following a Tracker

--- Assets/followWP.cs
+++ Assets/followWP.cs
@@ -10,15 +10,24 @@ public class followWP : MonoBehaviour
 
     public float speed = 10.0f;
     public float rotSpeed = 10.0f;
+    public float lookAhead = 10.0f;
+
+    GameObject tracker;
 
     void Start()
     {
+        tracker = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        DestroyImmediate(tracker.GetComponent<Collider>());
 
+        tracker.GetComponent<MeshRenderer>().enabled = false;
+
+        tracker.transform.position=this.transform.position;
+        tracker.transform.rotation=this.transform.rotation;
     }
 
-
-    void Update()
+    void ProgressTracker()
     {
+        if (Vector3.Distance(tracker.transform.position, this.transform.position) > lookAhead) return;
 
         if (Vector3.Distance(this.transform.position, waypoints[currentWP].transform.position) < 3)
         {
@@ -30,9 +39,18 @@ public class followWP : MonoBehaviour
             currentWP = 0;
         }
 
+        tracker.transform.LookAt(waypoints[currentWP].transform);
+        tracker.transform.Translate(0, 0, (speed+2)*Time.deltaTime);
+    }
+
+
+    void Update()
+    {
+        ProgressTracker();
+
         //this.transform.LookAt(waypoints[currentWP].transform);
 
-        Quaternion lookatWP = Quaternion.LookRotation(waypoints[currentWP].transform.position-this.transform.position);
+        Quaternion lookatWP = Quaternion.LookRotation(tracker.transform.position-this.transform.position);
 
         this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookatWP, rotSpeed * Time.deltaTime);
 
